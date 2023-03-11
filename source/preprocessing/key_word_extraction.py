bfcded2aaa255d05b2b8f8ac95f7f4226036b9e8
Alan Kniep <alan.kniep@studium.uni-hamburg.de>
2023-03-11 16:01:35 +0100

    Updated the preprocessing to except more commandline parameters

--- source/preprocessing/key_word_extraction.py
+++ source/preprocessing/key_word_extraction.py
@@ -1,51 +1,65 @@
 #!/usr/bin/env python3
 from keybert import KeyBERT
 
-class KeywordExtractor():
-    def __init__(self, n_keywords:int):
-        self.kw_model = KeyBERT()
+
+class KeywordExtractor:
+    """
+    Uses keyBERT to extract a list of Keywords for a given Document.
+    """
+
+    def __init__(self, documents:list, n_keywords: int, stop_words=None, min_length_of_keywords: int = 1, max_length_of_keywords: int = 1, model:str=None):
+        """
+        @param stop_words: list of strings of stop words or known string w.g. 'english'
+        @param int min_length_of_keywords: minimal number of words for a keyword
+        @param int max_length_of_keywords: maximal number of words for a keyword
+        """
+        self.kw_model = None
+        if model is None:
+            self.kw_model = KeyBERT()
+        else:
+            self.kw_model = KeyBERT(model=model)
+
         self.n_keywords = n_keywords
+        self.stop_words = stop_words
+        self.min_length_of_keywords = min_length_of_keywords
+        self.max_length_of_keywords = max_length_of_keywords
+        self.keywords_per_document = self.get_keywords(documents)
 
-
-    def get_keywords(self, document:str)->list:
+    def get_keywords(self, documents: list) -> list:
         """
         Generates keywords from 'article' and returns a list of dictionaries of the
-        form: [{"word": <keyword>, "similarity": <float>}, ...]
+        form: [[{"word": <keyword>, "similarity": <float>}], [...], ...]
         The similarity indicates how similar the keyword is to the article.
+
         @param str document: the document from which the Keywords should be extracted
         @return list
         """
         # Generate Keywords from the String article.
-        keywords = self.kw_model.extract_keywords(document, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=self.n_keywords)
-        
+        keywords_per_document = self.kw_model.extract_keywords(
+            documents,
+            keyphrase_ngram_range=(
+                self.min_length_of_keywords, self.max_length_of_keywords),
+                stop_words=self.stop_words,
+                top_n=self.n_keywords
+        )
+
         # Convert list of tuples to list of dictionaries
-        keyword_dicts = [{"word" : keyword[0], "similarity": keyword[1]} for keyword in keywords]
+        keyword_dicts = [[{"word": keyword[0], "similarity": keyword[1]}
+                         for keyword in document] for document in keywords_per_document]
 
         return keyword_dicts
 
-
-    def add_keywords(self, document:dict)->dict:
+    def add_keywords(self, document: dict, idx) -> dict:
         """
         Adds a new column with a list of keywords to the dataset.
         @param dict document: a row from the dataset to which the keywords should be added
         @return dict
         """
-        document["keywords"] = self.get_keywords(document['article_text'])
+        document["keywords"] = self.keywords_per_document[idx]
         return document
 
 
-def main(dataset:'dataset', n_keywords:int)->'dataset':
+def main(dataset: 'dataset', n_keywords: int, stop_words=None, min_length_of_keywords: int = 1, max_length_of_keywords: int = 1, model:str=None) -> 'dataset':
     print("### Keyword extraction:  Adding 'keywords' column to Dataset")
-    keyword_extractor = KeywordExtractor(n_keywords)
-    return dataset.map(keyword_extractor.add_keywords)
-    
-
-if __name__ == '__main__':
-    from datasets import load_dataset
-    dataset = load_dataset("wikipedia", "20220301.simple", split="train")
-
-    # reduce the ammount of data points
-    dataset = dataset.select(list(range(100)))
-    
-    updated_dataset = main(dataset, 3)
-    print(updated_dataset[0])
+    keyword_extractor = KeywordExtractor(dataset['article_text'], n_keywords, stop_words, min_length_of_keywords, max_length_of_keywords, model)
+    return dataset.map(keyword_extractor.add_keywords, with_indices=True)
--- source/preprocessing/main.py
+++ source/preprocessing/main.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
 import os
 import sys
-from datasets import load_dataset
-from datasets import load_from_disk
+from datasets import load_dataset, load_from_disk
+import create_dataset as create_ds
 import key_word_extraction as kwe
 import topic_modeling as tm
 
 
-def main(subset:str, data_points:int, nr_topics:int, n_keywords:int):
-    dataset = None
+def create_needed_directories():
+    """
+    Create import and export directory if not present
+    """
+    # import does not exist: create ist
+    if not os.path.exists("./import"):
+        print("### Preprocessing:  No import directory found; Creating import...")
+        os.makedirs("./import")
 
-    if os.path.isdir(f"./import/dataset-{subset}"):
-        print("### Preprocessing:  Found dataset on disk; Loading Dataset...")
-        dataset = load_from_disk(f"./import/dataset-{subset}")
-    # No dataset found in import
-    else:
-        # import does not exist: create ist
-        if not os.path.exists("./import"):
-            print("### Preprocessing:  No import directory found; Creating import...")
-            os.makedirs("./import")
-        # import exits but ist not a directory
-        elif not os.path.isdir("./import"):
-            sys.exit(f"{os.getcwd()}/import is not a directory")
-        # download dataset and save it to ./import
-        print("### Preprocessing:  Dataset localy not found; Downloading...")
-        dataset = load_dataset("wikipedia", subset, split="train")
-        print("### Preprocessing:  Saving Dataset to import/")
-        dataset.save_to_disk(f"./import/dataset-{subset}")
+    # import exits but ist not a directory
+    elif not os.path.isdir("./import"):
+        sys.exit(f"{os.getcwd()}/import is not a directory")
 
     # if export does not exist: create it
     if not os.path.exists("./export"):
@@ -34,6 +26,56 @@ def main(subset:str, data_points:int, nr_topics:int, n_keywords:int):
         os.makedirs("./export")
     elif not os.path.isdir("./export"):
         sys.exit(f"{os.getcwd()}/export is not a directory")
+
+
+def get_stop_words(file_name:str)->list:
+    """
+    Extract whitespace seperated stop words from file.
+    @param str: file_name, name of a file in /data/import containing the stop words
+    @return list
+    """
+    try:
+        with open(os.path.join("/data/import", file_name), 'r') as f:
+            lines = f.readlines()
+    except IOError:
+        sys.exit(f"{sys.argv[0]}: File '{file_name}' does not exist. Can't read stop words from file.")
+        
+    stop_words = "\n".join(lines)
+    return stop_words.split()
+
+
+def get_dataset(subset: str) -> 'dataset':
+    """
+    Get Wikipedia Dataset from Hugging Face
+    """
+    dataset = None
+
+    if os.path.isdir(f"./import/dataset-{subset}"):
+        print("### Preprocessing:  Found dataset on disk; Loading Dataset...")
+        dataset = load_from_disk(f"./import/dataset-{subset}")
+    # No dataset found in import
+    else:
+        # download dataset and save it to ./import
+        print("### Preprocessing:  Dataset localy not found; Downloading...")
+        dataset = load_dataset("wikipedia", subset, split="train")
+        print("### Preprocessing:  Saving Dataset to import/")
+        dataset.save_to_disk(f"./import/dataset-{subset}")
+
+    return dataset
+
+
+def main(subset: str, json_file:str, paperless_dir:str, data_points: int, nr_topics: int, n_keywords: int, stop_words:str, min_length_of_keywords:int, max_length_of_keywords:int, language:str, model:str):
+    """
+    Update Dataset and run topic modeling and keyword extration
+    """
+    create_needed_directories()
+    dataset = None
+    if subset is not None:
+        dataset = get_dataset(subset)
+    elif json_file is not None:
+        dataset = create_ds.from_json(json_file)
+    else:
+        dataset = create_ds.from_paperless_manifest(paperless_dir)
 
     # reduce the ammount of data points
     if data_points > 0:
@@ -46,48 +88,138 @@ def main(subset:str, data_points:int, nr_topics:int, n_keywords:int):
     dataset = dataset.rename_column("title", "heading")
     print("### Preprocessing:  Renaming 'text' column to 'article_text'...")
     dataset = dataset.rename_column("text", "article_text")
-    
-    updated_dataset = tm.main(dataset, nr_topics)
-    updated_dataset = kwe.main(updated_dataset, n_keywords)
+
+    updated_dataset = tm.main(dataset, nr_topics, language, stop_words, min_length_of_keywords, max_length_of_keywords)
+    updated_dataset = kwe.main(updated_dataset, n_keywords, stop_words, min_length_of_keywords, max_length_of_keywords, model)
 
     print("### Preprocessing:  Save updated Dataset to export/...")
     updated_dataset.save_to_disk("./export/article_data")
-    
-    
-    
+
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(
-        prog="Interactive Document Tagging: Preprocessing",
-        description='Preprocess the Wikipedia dataset for topic modeling'
+        prog="itd-preprocessing.py",
+        description="The preprocessing Script for the Interactive Document Tagging.\n" +
+            "It needs a .json file with a set of documents/articles, each with a " +
+            "title, the text and a url. Or a string which stands for a subset of " +
+            "the Wikipedia dataset from Hugging Face (https://huggingface.co/datasets/wikipedia)." +
+            "This script then creates or extends the dataset with the following " +
+            "features: topics with a label, a probability, and x and y coordinates " +
+            "of each document used for clustering. Furthermore, a set of keywords, " +
+            "which represent the respective document in the best possible way.",
+        epilog="Topic Modeling with BERTopic and KeyBERT"
     )
-    parser.add_argument('subset',
+    source = parser.add_mutually_exclusive_group(required=True)
+    source.add_argument('-w', '--wikipedia',
+        metavar="SUBSET",
         type=str,
-        help="a subset Dataset from the Wikipedia dataset (https://huggingface.co/datasets/wikipedia)",
-        default="20220301.simple"
+        help="The subset of the Wikipedia dataset (https://huggingface.co/datasets/wikipedia)",
+        default=None
+    )
+    source.add_argument('-j', '--json',
+        metavar="JSON FILE",
+        type=str,
+        help="A file path from the import directory to a json from which the Data should be parsed",
+        default=None
+    )
+    source.add_argument('-p', '--paperless',
+        metavar="MANIFEST.JSON",
+        type=str,
+        help="A file path relative to the import directory to a manifest.json " +
+            "created by a paperless export. See (https://docs.paperless-ngx.com)",
+        default=None
     )
     parser.add_argument('-n', '--number-data-points',
         metavar='n',
         action='store',
         type=int,
-        help="select only the first n datapoints of the dataset; use 0 to use all",
+        help="Select only the first n datapoints of the dataset. Set to 0 to use all (Default)",
         default=0
     )
     parser.add_argument('-t', '--topics',
         metavar='t',
         action='store',
         type=int,
-        help="The number of topics/clusters which should be created; 0 for a dynamic ammount",
+        help="If there BERTopic genereates more than t Topics let BERTopic iteratively " +
+            "merger the to most similar topics until there are only t Topics left." +
+            "Set to 0 for a dynamic ammount (Default)",
         default=0
     )
     parser.add_argument('-k', '--keywords',
-        metavar='t',
+        metavar='k',
         action='store',
         type=int,
-        help="The number of keywords per document which should be created",
+        help="Specifies that k keywords are to be created per document. Default: 5",
         default=5
+    )
+    parser.add_argument('--stop-words',
+        action='store',
+        metavar='STOP_WORDS',
+        type=str,
+        help="File path, relative to the import directory, to a space seperated " +
+            "list of stop words or the string 'english' for a pre defined list of " +
+            "english stop words which should be removed before the keywords are " +
+            "extracted or the topic representation is created. Default: None (don't" +
+            "remove stop words)",
+        default=None
+    )
+    parser.add_argument('--min',
+        metavar='MIN',
+        action='store',
+        type=int,
+        help="The minimal number of words per keywords for the keyword extraction. " +
+            "Default: 1",
+        default=1
+    )
+    parser.add_argument('--max',
+        metavar='MAX',
+        action='store',
+        type=int,
+        help="The maximal number of words per keywords for the keyword extraction. " +
+            "Default: 1",
+        default=1
+    )
+    parser.add_argument('-l', '--language',
+        action='store',
+        metavar='LANGUAGE',
+        type=str,
+        help="The language of the documents. E.g. 'english', 'german', 'multilingual'. " +
+            "Default: 'english'",
+        choices=["english", "multilingual", "german", 'afrikaans', 'albanian', 'amharic', 
+            'arabic', 'armenian', 'assamese', 'azerbaijani', 'basque', 'belarusian',
+            'bengali', 'bengali romanize', 'bosnian', 'breton', 'bulgarian', 'burmese',
+            'burmese zawgyi font', 'catalan', 'chinese (simplified)', 'chinese (traditional)',
+            'croatian', 'czech', 'danish', 'dutch', 'esperanto', 'estonian', 'filipino',
+            'finnish', 'french', 'galician', 'georgian', 'greek', 'gujarati', 'hausa',
+            'hebrew', 'hindi', 'hindi romanize', 'hungarian', 'icelandic', 'indonesian',
+            'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer',
+            'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian',
+            'lithuanian', 'macedonian', 'malagasy', 'malay', 'malayalam', 'marathi',
+            'mongolian', 'nepali', 'norwegian', 'oriya', 'oromo', 'pashto', 'persian',
+            'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'sanskrit',
+            'scottish gaelic', 'serbian', 'sindhi', 'sinhala', 'slovak', 'slovenian',
+            'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tamil', 'tamil romanize',
+            'telugu', 'telugu romanize', 'thai', 'turkish', 'ukrainian', 'urdu',
+            'urdu romanize', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'western frisian',
+            'xhosa', 'yiddish'],
+        default='english'
+    )
+    parser.add_argument('-L','--lemmatization',
+        action='store_true',
+        help="Should lemmatization be applied before the creation of the topic " +
+            "representation. Default: False",
+        default=False
     )
 
     args = parser.parse_args()
-    main(args.subset, args.number_data_points, args.topics, args.keywords)
+    stop_words = args.stop_words
+    if stop_words != "english" and stop_words is not None:
+        stop_words = get_stop_words(stop_words)
+
+    model = None
+    if args.language != 'english':
+        model = 'paraphrase-multilingual-MiniLM-L12-v2'
+    
+    main(args.wikipedia, args.json, args.paperless, args.number_data_points, args.topics, args.keywords, stop_words, args.min, args.max, args.language, model)
