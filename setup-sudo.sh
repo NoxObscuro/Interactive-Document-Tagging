b3797ea7666cd27a8d3d0e9d7e721a61546229fa
Alan Kniep <alan.kniep@studium.uni-hamburg.de>
2023-03-12 15:18:34 +0100

    Added a setup script for docker in userspace and renamed the other script to setup-sudo.sh

--- setup-sudo.sh
+++ setup-sudo.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "# Building 'idt-preprocessing' docker image... This might take several minutes."
 cd ${SCRIPT_DIR}/source/preprocessing
-docker build -t idt-preprocessing . 
+sudo docker build -t idt-preprocessing . 
 
 echo
 echo "# Building 'idt-app' docker image..."
 cd ${SCRIPT_DIR}/source/web-app
-docker build -t idt-app .
+sudo docker build -t idt-app .
 
 echo "# Creating database, import and export directory in ${SCRIPT_DIR}/application"
 mkdir -p ${SCRIPT_DIR}/application/{database,import,export}
@@ -23,5 +23,5 @@ echo "in the application directory to fit your needs. Read the README.md to get 
 echo "overview of the usage."
 echo
 echo "After customizing the docker-compose-preprocessing.yml run:"
-echo "docker compose -f ${SCRIPT_DIR}/application/docker-compose-preprocessing.yml up"
+echo "sudo docker compose -f ${SCRIPT_DIR}/application/docker-compose-preprocessing.yml up"
 echo "To start the preprocessing."
